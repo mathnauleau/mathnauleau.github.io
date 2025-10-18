5ea5472ab82a5228bb6197af94d6bb743de19c67
Mathieu <math.nauleau@gmail.com>
2025-10-18 10:33:45 +0200

    Cursor changes

--- js/localization.js
+++ js/localization.js
@@ -6,6 +6,15 @@ function updateContent(langData) {
     });
 }
 
+// Function to update the current flag based on language
+function updateCurrentFlag(lang) {
+    const currentFlag = document.getElementById('current-flag');
+    if (currentFlag) {
+        currentFlag.src = `assets/icon/${lang}.svg`;
+        currentFlag.alt = `${lang === 'en' ? 'english' : 'french'} flag`;
+    }
+}
+
 // Function to set the language preference
 function setLanguagePreference(lang) {
     localStorage.setItem('language', lang);
@@ -22,10 +31,15 @@ async function changeLanguage(lang) {
     setLanguagePreference(lang);
     const langData = await fetchLanguageData(lang);
     updateContent(langData);
+<<<<<<< Updated upstream
 
     // Check the associated radio button
     document.getElementById('option1').checked = (lang === 'fr');
     document.getElementById('option2').checked = (lang === 'en');
+=======
+    updateCurrentFlag(lang);
+    // toggleArabicStylesheet(lang); // Toggle Arabic stylesheet
+>>>>>>> Stashed changes
 }
 
 // Call changeLanguage() based on radio button selection
@@ -40,5 +54,13 @@ document.getElementById('option2').addEventListener('click', function() {
 // Call updateContent() on page load
 window.addEventListener('DOMContentLoaded', async () => {
     const userPreferredLanguage = localStorage.getItem('language') || 'en';
+<<<<<<< Updated upstream
     await changeLanguage(userPreferredLanguage);
 });
+=======
+    const langData = await fetchLanguageData(userPreferredLanguage);
+    updateContent(langData);
+    updateCurrentFlag(userPreferredLanguage);
+    toggleArabicStylesheet(userPreferredLanguage);
+}); 
+>>>>>>> Stashed changes
